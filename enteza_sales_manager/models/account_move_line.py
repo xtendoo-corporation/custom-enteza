700d870eb152f64e705c743dde75b12968c35438
Gokac9 <theabraham9@gmail.com>
2026-08-27 13:57:54 +0200

    sync update enteza_sales_manager

--- enteza_sales_manager/models/account_move_line.py
+++ enteza_sales_manager/models/account_move_line.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class AccountMoveLine(models.Model):
@@ -8,12 +8,25 @@ class AccountMoveLine(models.Model):
     sale_responsible_id = fields.Many2one(
         'res.users',
         string='Responsable de venta',
-        related='partner_id.sale_responsible_id',
+        compute='_compute_sale_responsible',
+        inverse='_inverse_sale_responsible',
         store=True,
         index=True,
-        readonly=False,
-        help='Usuario responsable de ventas heredado del asiento/factura.',
+        help='Usuario responsable de ventas vinculado al cliente de la línea contable.',
     )
+
+    @api.depends('partner_id', 'partner_id.sale_responsible_id')
+    def _compute_sale_responsible(self):
+        for line in self:
+            line.sale_responsible_id = line.partner_id.sale_responsible_id if line.partner_id else False
+
+    def _inverse_sale_responsible(self):
+        for line in self:
+            if line.partner_id and line.sale_responsible_id:
+                try:
+                    line.partner_id.sale_responsible_id = line.sale_responsible_id
+                except Exception:
+                    pass
 
 
 
